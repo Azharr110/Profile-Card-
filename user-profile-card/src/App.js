aef94c5f51f7ea922ef567a93e82aff47e387289
Azharr110 <abdazhar786@gmail.com>
2024-01-17 20:30:50 +0530

    react setup

---
+++ user-profile-card/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { UserProfileCard } from './Components/User-Profile-Card/UserProfileCard';
+
+function App() {
+  return (
+    <div className="App">
+        <UserProfileCard />
+    </div>
+  );
+}
+
+export default App;
