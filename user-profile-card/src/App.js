bb9f3af16b6713264976eb07566be27546c040a4
Azharr110 <abdazhar786@gmail.com>
2024-01-20 23:43:37 +0530

    Add 2nd profile

--- user-profile-card/src/App.js
+++ user-profile-card/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { UserProfileCard } from './Components/User-Profile-Card/UserProfileCard';
+import { UserProfileCardTwo } from './Components/User-Profile-Card/User2';
 
 function App() {
   return (
     <div className="App">
         <UserProfileCard />
+        <UserProfileCardTwo />
       
     </div>
   );
