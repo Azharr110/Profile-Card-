9421075960126e389e23f5c70333e58ae0e0507d
Azharr110 <abdazhar786@gmail.com>
2024-01-20 23:41:06 +0530

    Add profile data

---
+++ user-profile-card/src/Components/User-Profile-Card/User2.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./UserProfileCard.css"
+import profile_icon from "../Assets/Dp-bbx.jpg"
+
+export const UserProfileCardTwo = () => {
+  return (
+    <div className='upc'>
+       <div className="gradiant"></div>
+       <div className="profile-down">
+          <img src={profile_icon} alt="" />
+          <div className="profile-title">Syed Mohammad Ashar</div>
+          <div className="rpfile-description">
+          As a creative and analytical Software Developer, I possess excellent HTML, CSS, JavaScript, and React
+knowledge and skills. With an eye for detail and a team-player mentality, I excel at developing user-friendly
+and visually appealing web interfaces and thrive in fast-paced, team-driven environments.
+          </div>
+          <div className="profile-button">
+            <a href="mailto:abdazhar786@gmail.com">Contact Me</a>
+          </div>
+       </div>
+    </div>
+  )
+}
