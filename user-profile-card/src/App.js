b5c893c14152799c9c2e0de4515110561200b328
Azharr110 <abdazhar786@gmail.com>
2024-01-22 18:58:08 +0530

    Add user2 description

--- user-profile-card/src/App.js
+++ user-profile-card/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { UserProfileCard } from './Components/User-Profile-Card/UserProfileCard';
 import { UserProfileCardTwo } from './Components/User-Profile-Card/User2';
+import { UserProfileCardThree } from './Components/User-Profile-Card/User3';
 
 function App() {
   return (
     <div className="App">
         <UserProfileCard />
         <UserProfileCardTwo />
+        <UserProfileCardThree />
       
     </div>
   );
--- user-profile-card/src/Components/User-Profile-Card/User2.jsx
+++ user-profile-card/src/Components/User-Profile-Card/User2.jsx
@@ -10,9 +10,9 @@ export const UserProfileCardTwo = () => {
           <img src={profile_icon} alt="" />
           <div className="profile-title">Syed Mohammad Ashar</div>
           <div className="rpfile-description">
-          As a creative and analytical Software Developer, I possess excellent HTML, CSS, JavaScript, and React
-knowledge and skills. With an eye for detail and a team-player mentality, I excel at developing user-friendly
-and visually appealing web interfaces and thrive in fast-paced, team-driven environments.
+          Versatile Software Developer proficient in HTML, CSS, JavaScript, and React. Blend of creativity and analytical skills,
+           delivering visually appealing and intuitive web interfaces. Thrives in dynamic, 
+           team-driven environments with a keen eye for detail.
           </div>
           <div className="profile-button">
             <a href="mailto:abdazhar786@gmail.com">Contact Me</a>
